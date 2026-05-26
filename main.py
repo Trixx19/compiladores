708844a57fc512a710fc3272389fd05b0727e492
Any <anymelani@alu.ufc.br>
2026-05-25 22:09:24 -0300

    Gerar o DFA

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from expressaoRegular import tokens
 from geradorNFA import construir_nfa_global, coletar_todos_estados, resetar_contador
 
+
+from geradorDFA import nfa_para_dfa, testar_palavra
+
 def main():
     resetar_contador()
     
-    # Teste só com alguns tokens
+    
     tokens_teste = {
         "NUM"     : r"(num)",
         "NUM_LIT" : r"([0-9]+)",
@@ -12,15 +15,27 @@ def main():
         "EQ"      : r"(=)",
     }
 
-    nfa_global = construir_nfa_global(tokens_teste)  # passa o dict pro autômato
+    nfa_global = construir_nfa_global(tokens_teste)  
+    todos_estados_nfa = coletar_todos_estados(nfa_global)
 
-    todos = coletar_todos_estados(nfa_global)
-    finais = [e for e in todos if e.is_final]
+    estados_dfa, transicoes_dfa, estado_inicial_dfa, estados_finais_dfa = nfa_para_dfa(nfa_global, todos_estados_nfa)
 
-    print(f"Estados totais : {len(todos)}")
-    print(f"Estado inicial : {nfa_global.estado_inicial.id}")
-    for ef in finais:
-        print(f"  Estado {ef.id} => token='{ef.token}'")
+    print(f"O DFA foi criado e tem {len(estados_dfa)} estados no total.\n")
+    print("--- INICIANDO TESTES PRÁTICOS ---")
+
+    
+    palavras_para_testar = [
+        "num",          # Deve ser NUM
+        "12345",        # Deve ser NUM_LIT
+        "variavel_1",   # Deve ser VAR
+        "=",            # Deve ser EQ
+        "numeral",      # Deve ser VAR (Começa com num, mas não para por aí)
+        "123a"          # Deve dar Erro léxico
+    ]
+
+    for palavra in palavras_para_testar:
+        resultado = testar_palavra(palavra, estado_inicial_dfa, transicoes_dfa, estados_finais_dfa)
+        print(resultado)
 
 if __name__ == "__main__":
     main()
